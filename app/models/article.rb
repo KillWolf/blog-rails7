c0f2915813ba7feb8cb39bc6bfd75b4c2eb5e534
Jacob Pilegaard <jp@legalmonster.com>
2022-03-25 09:24:02 +0100

    starting authenticating

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,6 @@ class Article < ApplicationRecord
     default_scope -> { order(:created_at => :desc) }
 
     validates :title, length: {minimum: 5, maximum: 100}
+    validates :user_id, presence: true
     validates :description, length: {minimum: 10}
 end
